b27026619abff41f779436b406d233d53d255b78
Walesson Silva <walesson.silva.job@gmail.com>
2019-07-28 14:31:19 -0300

    [DEV] - Git initial

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# schedulerjs
-Scheduler used for NODE RED module
+# Schedulerjs
+
+##### Scheduler used for CRON module
+#
+
+>> The payload exemple received
+#
+```javascript
+// Exemplo object receive
+{
+    target          : 22,
+    stripMin        : 10,
+    stripMax        : 25,
+    day             : 5,
+    enableDay       : 1,
+    timeSwitchedOn  : '09:41',
+    timeSwitchedOff : '16:00'
+}
+```
+___
+#
+>> The sample payload sent
+#
+```javascript
+// Exemplo object sent
+{
+    target   : 22,
+    stripMin : 10,
+    stripMax : 25,
+    on       : 1
+}
+```
+___
+#
+#
+###### Develop by *Walesson Silva*
---
+++ scheduler.js
@@ -0,0 +1,87 @@
+/**
+ * Scheduler
+ * @date 02/07/2018
+ * @author Walesson Silva
+ */
+
+class Scheduler extends Date {
+    constructor(params){
+        super();
+        this.target     = params.target;
+        this.stripMin   = params.stripMin;
+        this.stripMax   = params.stripMax;
+    }
+    
+    setSchedule(params) {
+        let currentday = this.getDay();
+        let disableDay = JSON.parse(params.enableDay);
+        disableDay = (disableDay == 1) ? true : false;
+        
+        if(currentday == params.day && disableDay) {
+            this.checkTimes(params);
+        }
+    }
+    
+    checkTimes(params) {
+        let currentTime = String(this.getHours()).padStart(2, '0')+':'+String(this.getMinutes()).padStart(2, '0');
+        let sender = {
+            target   : this.target,
+            stripMin : this.stripMin,
+            stripMax : this.stripMax,
+            on       : null
+        };
+        
+        params.timeSwitchedOn.forEach((item, index) => {
+            if(item == currentTime){
+                sender.on = 1;
+                this.sendPayload(sender);
+            }
+        });
+        
+        params.timeSwitchedOff.forEach((item, index) => {
+            if(item == currentTime){
+                sender.on = 0;
+                this.sendPayload(sender);
+            }
+        });
+    }
+    
+    sendPayload(data) {
+        node.send({
+            payload: data
+        })
+    }
+    
+};
+
+msg.payload = msg.payload.replace(/\\/g , "");
+
+if(!(msg.payload).length) {
+    msg.payload = null;
+    return false;
+}
+
+msg.payload = JSON.parse(msg.payload);
+
+if(!msg.payload.hasOwnProperty('params')) {
+    msg.payload = null;
+    return false;
+}
+
+let param = {
+    target       : msg.payload.target,
+    stripMin     : msg.payload.stripMin,
+    stripMax     : msg.payload.stripMax
+};
+
+global.set("target", msg.payload.target);
+global.set("stripMin", msg.payload.stripMin || 1);
+global.set("stripMax", msg.payload.stripMax || 1);
+
+let sc = new Scheduler(param);
+
+for(let item of msg.payload.params) {
+    sc.setSchedule(item);
+};
+
+
